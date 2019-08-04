217d4ecfcb32fd6155f099b9881aff72c764b9ab
dipeshsonawale <dipesh.sonawale@gmail.com>
2019-08-03 22:22:24 -0400

    Commit for dev branch

--- technotes/src/main/java/com/work/TechnotesApplication.java
+++ technotes/src/main/java/com/work/TechnotesApplication.java
@@ -8,7 +8,7 @@ public class TechnotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechnotesApplication.class, args);
-		System.out.println("Hello World !!!!!!");
+		System.out.println("Hello World Dev !!!");
 	}
 
 }
